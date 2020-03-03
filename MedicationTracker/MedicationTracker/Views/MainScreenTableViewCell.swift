4e4c7f81978a3df632cf743513757b279c7a4375
Hector L <hectoremil21@hotmail.com>
2020-03-03 14:47:56 -0500

    Create Cocoa class for MainView's table cell and assign it.

---
+++ MedicationTracker/MedicationTracker/Views/MainScreenTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainScreenTableViewCell.swift
+//  MedicationTracker
+//
+//  Created by Karen Rodriguez on 3/3/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
