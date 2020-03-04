69642300056797c5a240536d6fa95c6385eeb942
Hector L <hectoremil21@hotmail.com>
2020-03-04 10:21:04 -0500

    Add and assign custom cell classes for tables in DetailView

---
+++ MedicationTracker/MedicationTracker/Views/ExistingItemsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ExistingItemsTableViewCell.swift
+//  MedicationTracker
+//
+//  Created by Karen Rodriguez on 3/4/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class ExistingItemsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var itemNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
