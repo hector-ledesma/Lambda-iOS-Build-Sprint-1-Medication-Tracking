fde68d129f200ff669a9bba85948e0c5012b8e71
Hector L <hectoremil21@hotmail.com>
2020-03-03 19:55:47 -0500

    test

---
+++ MedicationTracker/MedicationTracker/Models/IdentifierProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  IdentifierProtocol.swift
+//  MedicationTracker
+//
+//  Created by Karen Rodriguez on 3/3/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+
+protocol Identifier {
+    var name: String { get }
+}
