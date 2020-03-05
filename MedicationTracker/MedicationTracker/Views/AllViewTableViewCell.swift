56e96a3cb43a85789b2adb3ebba4ca5eb5024e84
Hector L <hectoremil21@hotmail.com>
2020-03-05 18:04:20 -0500

    Create one single custom table view cell cocoa class that both all views will conform to.

---
+++ MedicationTracker/MedicationTracker/Views/AllViewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AllViewTableViewCell.swift
+//  MedicationTracker
+//
+//  Created by Karen Rodriguez on 3/5/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class AllViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
