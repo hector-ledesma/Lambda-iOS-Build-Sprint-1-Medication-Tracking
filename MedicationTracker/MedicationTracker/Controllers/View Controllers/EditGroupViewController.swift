699facac5fb4d68cd52d72318f580445d0fec391
Hector L <hectoremil21@hotmail.com>
2020-03-15 17:44:33 -0400

    Create outlets for edit group view.

--- MedicationTracker/MedicationTracker/Controllers/View Controllers/EditGroupViewController.swift
+++ MedicationTracker/MedicationTracker/Controllers/View Controllers/EditGroupViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class EditGroupViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var groupNameLabel: UITextField!
+    @IBOutlet weak var addedItemsTableView: UITableView!
+    @IBOutlet weak var existingItemsTableView: UITableView!
+    @IBOutlet weak var alertLabel: UILabel!
+    @IBOutlet weak var alertSwitch: UISwitch!
+    
+    // MARK: - Properties
+    
+    var group: Group?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
